03d6f236a79247d8dc6b7153750c326fe6c1588d
James Brattin <james.brattin@proton.me>
2024-03-28 16:42:20 -0700

    zip and upload lambda to new bucket

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Learning Terraform
 
 Repo for learning terraform
+
+## Milestones
+
+Created an S3 bucket
+
+WIP: Create S3 bucket. Then, zip up and upload lambda function to bucket
--- providers.tf
+++ providers.tf
@@ -2,6 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+      version = "5.42.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.4.2"
     }
   }
 }
